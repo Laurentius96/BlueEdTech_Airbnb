0767394156b2d9b5a0ef753d2e4fa1848550b1ce
Lorenzo <bianchi92@hotmail.com>
2022-01-27 23:47:29 -0300

    Add segurança

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+// 38°) Importando o Prima Service, JWTModule, jwtContants, PassportModule...
+import { PrismaService } from 'src/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './jwt.constants';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, PrismaService],
 })
 export class AuthModule {}
