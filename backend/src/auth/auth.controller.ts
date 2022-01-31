db7a10796fd85362d72083c77a43f0f3270d14bb
Lorenzo <bianchi92@hotmail.com>
2022-01-31 19:54:24 -0300

    finalizando o sistema de segurança da aplicação

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 // 51°) Importando...
 import { LoginInputDto } from './dto/login-input.dto';
 // 56°)
 import { LoginResponseDto } from './dto/login-response.dto';
+// 58°)
+import { Request } from 'express';
+// 59°) Após colocar o UseGuard na importação...
+import { AuthGuard } from '@nestjs/passport';
+// 65°) Importando o LoggedUser...
+import { LoggedUser } from './logged-user.decorator';
+// 67°) Importando o User...
+import { User } from '@prisma/client';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -12,5 +20,14 @@ export class AuthController {
   @Post()
   login(@Body() loginInputDto: LoginInputDto): Promise<LoginResponseDto> {
     return this.authService.login(loginInputDto);
+  }
+
+  // 57°)
+  @Get()
+  // 60°) Colocando o UseGuards...
+  @UseGuards(AuthGuard())
+  // 66°)
+  me(@LoggedUser() user: User) {
+    return user;
   }
 }
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 // 13°)Importando o CreateUserDto...
@@ -16,7 +17,10 @@ import { User } from '@prisma/client';
 import { UserDto } from './dto/user.dto';
 // 33°) Importando o UpdateUserDato...
 import { UpdateUserDto } from './dto/update-user.dto';
-
+// 70°) Após colocar o UseGuards na importação acima linha 1, importamos o UseGuard...
+import { AuthGuard } from '@nestjs/passport';
+// 75°) Importando LoggedUser...
+import { LoggedUser } from 'src/auth/logged-user.decorator';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -30,6 +34,8 @@ export class UserController {
 
   // 20°) Criando mais um método...
   @Get()
+  // 71°) Add o @UseGuards(AuthGuard())...
+  @UseGuards(AuthGuard())
   // 26°) Add o {UserDto}...
   findMany(): Promise<UserDto[]> {
     return this.userService.findMany();
@@ -37,22 +43,34 @@ export class UserController {
 
   // 28°)
   @Get(':id')
+  // 72°) Add o @UseGuards(AuthGuard())...
+  @UseGuards(AuthGuard())
   findUnique(@Param('id') userId: string): Promise<User> {
     return this.userService.findUnique(userId);
   }
 
   // 34°)
-  @Patch(':id')
+  // 76°) Retiramos o id do @Patch(':id')...
+  @Patch()
+  // 73°) Add o @UseGuards(AuthGuard())...
+  @UseGuards(AuthGuard())
   update(
-    @Param('id') userId: string,
+    // 77°) Deletamos o "@Param('id') userId: string," e colocamos no lugar o '@LoggedUser() user: User'
+    @LoggedUser() user: User,
     @Body() updateUserDto: UpdateUserDto,
   ): Promise<User> {
-    return this.userService.update(userId, updateUserDto);
+    // 78°) Trocamos o 'userId' detrono dos () por 'user.id'
+    return this.userService.update(user.id, updateUserDto);
   }
 
   // 36°)
-  @Delete(':id')
-  delete(@Param('id') userId: string) {
-    return this.userService.delete(userId);
+  // 79°) Retiramos o id do @Delete(':id')...
+  @Delete()
+  // 80°) Add o @UseGuards(AuthGuard())...
+  @UseGuards(AuthGuard())
+  // 81°) Retiramos o "@Param('id') userId: string," dentro () e colocamos no lugar o '@LoggedUser() user: User'
+  delete(@LoggedUser() user: User) {
+    // 82°) Trocamos o 'userId' detrono dos () por 'user.id'
+    return this.userService.delete(user.id);
   }
 }
